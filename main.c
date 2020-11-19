b18524d8764937783f207511a39dfd63d36c6144
yeongseo724 <70522995+yeongseo724@users.noreply.github.com>
2020-11-19 15:46:02 +0900

    12 coding(4)

--- main.c
+++ main.c
@@ -6,12 +6,9 @@
 
 int main(int argc, char *argv[]) {
 	
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	char str[30]= "happy C programming";
 	
-	strcpy(dst, src);
-	
-	printf("copied string : %s\n", dst);
+	printf("length of \"%s\" : %i\n", str, strlen(str));
 			
 	return 0;
 }
